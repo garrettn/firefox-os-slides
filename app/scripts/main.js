ea0bdfc215cee0f7df600ffce037333dbbf14cff
Garrett Nay <garrettnay@ymail.com>
2015-04-25 15:23:02 -0600

    Install Reveal.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,4 @@
 /* jshint devel:true */
+Reveal.initialize({
+
+});
--- gulpfile.js
+++ gulpfile.js
@@ -78,6 +78,7 @@ gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 gulp.task('serve', ['styles', 'fonts'], function () {
   browserSync({
     notify: false,
+    open: false,
     port: 9000,
     server: {
       baseDir: ['.tmp', 'app'],
